2b40a4f03b91d1b166a355caf01ee68fc6b81334
plius <92224199+Plius1@users.noreply.github.com>
2021-10-16 20:19:08 +0300

    ivesti name fixai
    sufixintai ifai ivesti name kad kai ivedamas tuscias name neleistu startinti

--- app/src/main/java/com/ifk8edvmat1gabkojginkir/ktuprojektas/MainActivity.kt
+++ app/src/main/java/com/ifk8edvmat1gabkojginkir/ktuprojektas/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.decorView.systemUiVisibility=View.SYSTEM_UI_FLAG_FULLSCREEN
-        val btn_start= findViewById<View>(R.id.btnEnter)
-        val et_name= findViewById<View>(R.id.etName)
+        val btn_start : Button = findViewById(R.id.btnEnter)
+        val et_name : EditText = findViewById(R.id.etName)
+
+        //val msg2 = et_name.toString().trim()
+       // val msg: String = et_name.toString()
+
         btn_start.setOnClickListener {
 
-            if(et_name.toString().isEmpty()) {
-                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show() //nivalit
-            } else {
+            val user_error: String = et_name.text.toString()
+            if(user_error.trim().isEmpty()){
+                et_name.error = "Required"
+               Toast.makeText(applicationContext, "Please enter your desired name", Toast.LENGTH_SHORT).show() //veikia sutvarkiau
+            }
+            else{
+                Toast.makeText(applicationContext, "successful", Toast.LENGTH_SHORT).show() //veikia sutvarkiau
                 val intent = Intent(this, QuizMainActivty::class.java)
                 startActivity(intent)
                 finish()
@@ -26,3 +36,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 }
+
+
+
