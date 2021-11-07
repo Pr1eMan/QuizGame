d612f3ccd245a220d96a862b74aad167568ece0b
Gabrielius_Kojelis <31181288+Pr1eMan@users.noreply.github.com>
2021-11-07 17:30:25 +0200

    pridetas night mode funkcija

--- app/src/main/java/com/ifk8edvmat1gabkojginkir/ktuprojektas/SettingsActivity.kt
+++ app/src/main/java/com/ifk8edvmat1gabkojginkir/ktuprojektas/SettingsActivity.kt
@@ -1,10 +1,14 @@
 package com.ifk8edvmat1gabkojginkir.ktuprojektas
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +20,8 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .registerOnSharedPreferenceChangeListener(this)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
@@ -23,4 +29,25 @@ class SettingsActivity : AppCompatActivity() {
            setPreferencesFromResource(R.xml.root_preferences, rootKey)
        }
    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if(key == "NUGHT"){
+            val prefs = sharedPreferences?.getBoolean(key,false)
+            when(prefs?.toString()){
+                "false" ->{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                "true" ->{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .unregisterOnSharedPreferenceChangeListener(this)
+    }
 }
