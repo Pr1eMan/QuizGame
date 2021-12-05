102d444f9bda41c4db63acda4de3f958e9c6ae1c
plius <92224199+Plius1@users.noreply.github.com>
2021-12-05 19:25:03 +0200

    Revert "Revert "User can now select topic for wiki questionaire.""
    This reverts commit 3918955b0dd82eb2182371b3fe13efc014c3f11b.

--- app/src/main/java/com/ifk8edvmat1gabkojginkir/ktuprojektas/RetrofitClientInstance.kt
+++ app/src/main/java/com/ifk8edvmat1gabkojginkir/ktuprojektas/RetrofitClientInstance.kt
@@ -2,21 +2,30 @@ package com.ifk8edvmat1gabkojginkir.ktuprojektas
 
 import retrofit2.Retrofit
 import com.ifk8edvmat1gabkojginkir.ktuprojektas.RetrofitClientInstance
+import okhttp3.OkHttpClient
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClientInstance {
     private var retrofit: Retrofit? = null
     private const val BASE_URL =
         "https://itoaze.lt:4443/"  // getquizservice ten galas linko uzrasyti tinkamai
+    private val client = OkHttpClient.Builder().build()
     val retrofitInstance: Retrofit?
         get() {
             if (retrofit == null) {
 
-                retrofit = Retrofit.Builder().baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create()).build()
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build()
             }
             return retrofit
         }
+
+    fun<T> buildService(service: Class<T>): T? {
+        return retrofitInstance?.create(service)
+    }
 }
 
 
